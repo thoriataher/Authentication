4ae45506479b34b1e8303972367fc800cc96179b
Thoria <tahertaher6600@gmail.com>
2024-01-14 15:36:23 +0100

    Level 2 Encryption

--- app.js
+++ app.js
@@ -12,11 +12,13 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 mongoose.connect("mongodb://localhost:27017/userDB");
 
-const userSchema ={ 
+const userSchema = new mongoose.Schema({ 
     email: String,
     password: String
-};
+});
 
+const secret = "Thisisourlittlesecret."; 
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
 
 
 const User = new mongoose.model('User', userSchema);
